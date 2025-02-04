6560117fd2c6ff2f8a7ed1ca34088320ba4cca13
Awes Khan <akhan@remwes.com>
2025-02-04 18:41:30 +0530

    btn4

--- src/components/PopupForm.jsx
+++ src/components/PopupForm.jsx
@@ -89,13 +89,15 @@ const PopupForm = ({ isOpen, onClose, onSubmit, isEditConfirmation, editData, on
                                       className="w-full p-2 border border-gray-300 rounded"
                                       placeholder="Event Location"
                                       required
-                                  />
+                              />
+                                <div className="mm-popup__box__footer__right-space">
                                   <button type="button" onClick={onClose} className="bg-gray-400 text-white px-4 py-2 rounded">
                                       Cancel
                                   </button>
-                                  <button type="submit" className="mm-popup__box__footer__right-space mm-popup__btn bg-green-400 text-white px-4 py-2 rounded hover:bg-blue-700">
+                                  <button type="submit" className=" mm-popup__btn bg-green-400 text-white px-4 py-2 rounded hover:bg-blue-700">
                                       {editData ? "Update" : "Save"}
                                   </button>
+                                </div>
                           </form>
                       </div>
                   )
